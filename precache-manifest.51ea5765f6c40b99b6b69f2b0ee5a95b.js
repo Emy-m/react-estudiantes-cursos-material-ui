6c6fbcdd3cce32356266c9138f1bd5023734c0a2
Emy-m <emiunrn@gmail.com>
2022-04-17 20:34:46 -0300

    Updates

---
+++ precache-manifest.51ea5765f6c40b99b6b69f2b0ee5a95b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba6a2007154ee8253b22",
+    "url": "/react-estudiantes-cursos-material-ui/static/css/main.9cefdf13.chunk.css"
+  },
+  {
+    "revision": "ba6a2007154ee8253b22",
+    "url": "/react-estudiantes-cursos-material-ui/static/js/main.ba6a2007.chunk.js"
+  },
+  {
+    "revision": "343bf09345c6a290254a",
+    "url": "/react-estudiantes-cursos-material-ui/static/js/1.343bf093.chunk.js"
+  },
+  {
+    "revision": "c54f6d8778ff9eacdedb",
+    "url": "/react-estudiantes-cursos-material-ui/static/js/2.c54f6d87.chunk.js"
+  },
+  {
+    "revision": "b9ceef1ac1886a4aab81",
+    "url": "/react-estudiantes-cursos-material-ui/static/js/runtime~main.b9ceef1a.js"
+  },
+  {
+    "revision": "8e256bb91ce15f5b46063af657339d1a",
+    "url": "/react-estudiantes-cursos-material-ui/index.html"
+  }
+];
